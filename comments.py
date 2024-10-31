6f2f5f81877486e23f2c77494c7bc128ada5c3a4
panchamnarng30 <132580036+panchamnarng30@users.noreply.github.com>
2024-10-31 15:10:09 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,19 @@
+
+'''
+sdfgxfbggggggggggggggggggggggggggv
+dsgjoUHOOOOOOOidskifsdiofkdosnkn
+ioljnhlodslsnloklo;pmpkijmjkmmklmk
+lmlm,svksm;dfm,l;pkvsm,;kdfpsnkp
+(This is multiline comment)
+'''
+
+"""
+sdfgxfbggggggggggggggggggggggggggv
+dsgjoUHOOOOOOOidskifsdiofkdosnkn
+ioljnhlodslsnloklo;pmpkijmjkmmklmk
+lmlm,svksm;dfm,l;pkvsm,;kdfpsnkp
+(This is multiline comment)
+"""
+
+
+# This prints a random (This is single line comment)
---
+++ modules.py
@@ -0,0 +1,17 @@
+import pyjokes
+
+print("Printing Jokes....")
+
+# This prints a random (This is single lne comment)
+
+joke = pyjokes.get_joke
+print(joke)
+
+
+'''
+
+
+dsgjoUHOOOOOOOidskifsdiofkdosnknioljnhlodslsnloklo;pmpkijmjkmmklmklmlm,svksm;dfm,l;pkvsm,;kdfpsnkp
+
+(This is multiline comment)
+'''
